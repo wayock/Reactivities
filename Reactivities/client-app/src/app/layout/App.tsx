91442975fdc8fe182713cd356f297efddc767dbd
Matthew Wayock <wayock@gmail.com>
2020-11-18 23:31:23 -0500

    Initial React UI setup

---
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import { Header, Icon, List } from 'semantic-ui-react'
+import axios from 'axios';
+import { IActivity } from '../models/activity';
+import NavBar from '../../features/nav/NavBar';
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>('http://localhost:5000/api/activities')
+      .then((response) => {
+        setActivities(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+       <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default App;
