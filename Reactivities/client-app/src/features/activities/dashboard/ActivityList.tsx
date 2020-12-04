3dec473164b926fce8c1ced34f565f4c422530a1
Matthew Wayock <wayock@gmail.com>
2020-12-03 22:09:44 -0500

    added axios for http client

--- Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,49 +1,65 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 
 interface IProps {
     activities: IActivity[];
     selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
+    deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+    submitting: boolean;
+    target: string;
 }
 
-const ActivityList: React.FC<IProps> = ({activities, selectActivity, deleteActivity}) => {
-    return (
-      <div>
-        <Segment clearing>
-          <Item.Group divided>
-            {activities.map((activity) => (
-              <Item key={activity.id}>
-                <Item.Content>
-                  <Item.Header as="a">{activity.title}</Item.Header>
-                  <Item.Meta>{activity.date}</Item.Meta>
-                  <Item.Description>
-                    <div>{activity.description}</div>
-                    <div>{activity.city}, {activity.venue}</div>
-                  </Item.Description>
-                  <Item.Extra>
-                    <Button 
-                      onClick={() => selectActivity(activity.id)}
-                      floated="right" 
-                      content="View" 
-                      color="blue" 
-                    />
-                    <Button 
-                      onClick={() => deleteActivity(activity.id)}
-                      floated="right" 
-                      content="Delete" 
-                      color="red" 
-                    />
-                    <Label style={{textTransform: 'capitalize'}} basic content={activity.category} />
-                  </Item.Extra>
-                </Item.Content>
-              </Item>
-            ))}
-          </Item.Group>
-        </Segment>
-      </div>
-    );
-}
+const ActivityList: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  deleteActivity,
+  submitting,
+  target
+}) => {
+  return (
+    <div>
+      <Segment clearing>
+        <Item.Group divided>
+          {activities.map((activity) => (
+            <Item key={activity.id}>
+              <Item.Content>
+                <Item.Header as="a">{activity.title}</Item.Header>
+                <Item.Meta>{activity.date}</Item.Meta>
+                <Item.Description>
+                  <div>{activity.description}</div>
+                  <div>
+                    {activity.city}, {activity.venue}
+                  </div>
+                </Item.Description>
+                <Item.Extra>
+                  <Button
+                    onClick={() => selectActivity(activity.id)}
+                    floated="right"
+                    content="View"
+                    color="blue"
+                  />
+                  <Button
+                    name={activity.id}
+                    loading={target === activity.id && submitting}
+                    onClick={(e) => deleteActivity(e, activity.id)}
+                    floated="right"
+                    content="Delete"
+                    color="red"
+                  />
+                  <Label
+                    style={{ textTransform: "capitalize" }}
+                    basic
+                    content={activity.category}
+                  />
+                </Item.Extra>
+              </Item.Content>
+            </Item>
+          ))}
+        </Item.Group>
+      </Segment>
+    </div>
+  );
+};
 
 export default ActivityList
--- Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
+++ Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,8 @@ interface IProps {
     activity: IActivity
     createActivity: (activity: IActivity) => void;
     editActivity: (activity: IActivity) => void;
+    submitting: boolean;
+    target: string;
 }
 
 const ActivityForm: React.FC<IProps> = ({
@@ -15,6 +17,7 @@ const ActivityForm: React.FC<IProps> = ({
   activity: initialFormState,
   createActivity,
   editActivity,
+  submitting
 }) => {
   const initializeForm = () => {
     if (initialFormState) {
@@ -94,7 +97,7 @@ const ActivityForm: React.FC<IProps> = ({
           placeholder="Venue"
           value={activity.venue}
         />
-        <Button floated="right" positive type="submit" content="Submit" />
+        <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
         <Button
           onClick={() => setEditMode(false)}
           floated="right"
